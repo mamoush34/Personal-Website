39fd4f8a802cba9202ffd0bbe8e037fa56096f17
Mohammad Amoush <47069173+mamoush34@users.noreply.github.com>
2020-09-13 21:19:42 -0400

    Pop-up and constants added

---
+++ src/client/models/constants.ts
@@ -0,0 +1,51 @@
+export const doodleDesc: string = "";
+export const doodleUrl: string = "";
+export const doodleOverview: string = "";
+
+export const tetrisDesc: string = "";
+export const tetrisUrl: string = "";
+export const tetrisOverview: string = "";
+
+export const pacmanDesc: string = "";
+export const pacmanUrl: string = "";
+export const pacmanOverview: string = "";
+
+export const cShellDesc: string = "";
+export const cShellUrl: string = "";
+export const cShellOverview: string = "";
+
+export const mallocDesc: string = "";
+export const mallocUrl: string = "";
+export const mallocOverview: string = "";
+
+export const databaseDesc: string = "";
+export const databaseUrl: string = "";
+export const databaseOverview: string = "";
+
+export const autocorrectDesc: string = "";
+export const autocorrectUrl: string = "";
+export const autocorrectOverview: string = "";
+
+export const mapsDesc: string = "";
+export const mapsUrl: string = "";
+export const mapsOverview: string = "";
+
+export const UDDUDesc: string = "";
+export const UDDUUrl: string = "";
+export const UDDUOverview: string = "";
+
+export const featureDesc: string = "";
+export const featureUrl: string = "";
+export const featureOverview: string = "";
+
+export const sceneDesc: string = "";
+export const sceneUrl: string = "";
+export const sceneOverview: string = "";
+
+export const inTurnDesc: string = "";
+export const inTurnUrl: string = "";
+export const inTurnOverview: string = "";
+
+export const camDesc: string = "";
+export const camUrl: string = "";
+export const camOverview: string = "";
--- src/client/views/project_popup.tsx
+++ src/client/views/project_popup.tsx
@@ -22,7 +22,14 @@ export default class ProjectPopUp extends React.Component<PopUpInterface> {
                     <div className="pop_image">
                         <img src={cur_project.imageUrl} alt={cur_project.name}/>
                     </div>
-                    <div className="pop_desc">{cur_project.description}</div>
+                    <div className="pop_desc">
+                        <div className="info_box">
+                            <h1>{cur_project.name}</h1>
+                            <p>{cur_project.description}</p>
+                            <h5>Technologies Used</h5>
+                            <p>{cur_project.technologies.toString()}</p>
+                        </div>
+                    </div>
                 </div>
                 <button title="Close (Esc)" type="button" className="pop_close" onClick={this.props.deSelect}>×</button>
             </div>
