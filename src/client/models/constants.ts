ea8cf9217c9d8014656628d738c4419baa54222d
Mohammad Amoush <47069173+mamoush34@users.noreply.github.com>
2020-09-16 18:47:16 -0400

    descriptions updated

--- src/client/models/constants.ts
+++ src/client/models/constants.ts
@@ -1,38 +1,38 @@
-export const doodleDesc: string = "An implementation of the popular game DoodleJump, except in this we have a monkey trying to climb in halloween. It's coded in Java, using Object-Oriented Programming principles";
+export const doodleDesc: string = "An implementation of the popular game DoodleJump, except in this we have a monkey trying to climb during halloween. The project is coded in Java, using Object-Oriented Programming principles.";
 export const doodleUrl: string = "/images/doodle.png";
 
-export const pacmanDesc: string = "An implementation of the game Pacman, with increasing difficulty. Side Map transitions are enabled. The ghosts use BFS algorithm to chase Pacman, so that the game is actually challenging to users. Every known Pacman feature such as energized pacman is implemented as well";
+export const pacmanDesc: string = "An implementation of the game Pacman, with increasing difficulty. Side Map transitions are enabled. The ghosts use BFS algorithm to chase Pacman, so that the game is actually challenging to users. Every Pacman feature in the classic game, such as energizer(ghosts becoming eadible and scared) is implemented as well. Object Oriented Principles, such as Polymorphism, Inheritence, Encapsulation are obeyed.";
 export const pacmanUrl: string = "/images/pacman.png";
 
-export const cShellDesc: string = "A personal implementation of a shell coded in C. The shell has ability to handle unix built-in system calls, child and parent processes, command line input, as well as file handling(descriptors etc.) Besides, the shell is also able to handle multiple processes by being able to handle both foreground and background processes with reaping that results in no leaks";
+export const cShellDesc: string = "A personal implementation of a shell coded in C. The shell has ability to handle unix built-in system calls, child and parent processes, command line input, as well as file handling(descriptors etc.). Besides, the shell is also able to handle multiple processes by being able to handle both foreground and background processes with reaping that results in no leaks.";
 export const cShellUrl: string = "/images/cshell.png";
 
-export const mallocDesc: string = "A personal implementation of a dynamic memory allocator for C programs. It's implemented using first-fit explicit free list approach";
+export const mallocDesc: string = "A personal implementation of a dynamic memory allocator for C programs. It's implemented using first-fit explicit free list approach.";
 export const mallocUrl: string = "/images/malloc.png";
 
-export const databaseDesc: string = "In this project, you are able to create a simple server to manage a database of key-value pairs over a network. Multiple concurrent users are able to search for items in the database, add new entries and remove existing entries, all while making sure of no data race with proper mutex usage";
+export const databaseDesc: string = "In this project, you are able to create a simple server to manage a database of key-value pairs over a network. Multiple concurrent users are able to search for items in the database, add new entries and remove existing entries, all while making sure of no data race happens, with proper lock usage.";
 export const databaseUrl: string = "/images/database.png";
 
-export const mapsDesc: string = "Created a navigation app that draws a map on a canvas, loaded from a database, and lets users find a way to their target. Implemented K-D Tree to find roads that fall inside a box of 4 coordinates, or closest to a point. •	The roads are painted depending on the traffic values received through server, and Dijkstra implementation takes traffic into account while giving the shortest path.";
+export const mapsDesc: string = "Created a navigation app that draws a map on a canvas, loaded from a database, and lets users find a way to their target. Implemented K-D Tree to find roads that fall inside a box of 4 coordinates, or closest to a point. The roads are painted depending on the traffic values received through server, and Dijkstra implementation takes traffic into account while giving the shortest path. The road names are suggested to user as they're typing with help of my AutoCorrect implementation that relis on an implemented Trie structure.";
 export const mapsUrl: string = "/images/maps.png";
 
-export const UDDUDesc: string = "Collaborated to develop a Start-Up web application that allows users to share travel experiences, activities, and itineraries. Users could browse shared activities based on filters on the city selected. Implemented itinerary creation for users using a cyclic non-deterministic selection algorithm that tried to match preferences on criteria like budget, time, etc. The itinerary could be displayed in various ways including on a map, which was accomplished through Google Maps API. Incorporated persistent data storage for data including credentials through SQLite. Leveraged caching for recently searched activities or the cities`";
+export const UDDUDesc: string = "Collaborated to develop a Start-Up web application that allows users to share travel experiences, activities, and itineraries. Users could browse shared activities based on filters on the city selected. Implemented itinerary creation for users using a cyclic non-deterministic selection algorithm that tried to match preferences on criteria like budget, time, etc. The itinerary could be displayed in various ways including on a map, which was accomplished through Google Maps API. Incorporated persistent data storage for data including credentials through SQLite. Leveraged caching for recently searched activities or the cities. Data stored is user-specific, which is handled by a credential system backed by Passport.js.";
 export const UDDUUrl: string = "/images/UDDU.png";
 
-export const featureDesc: string = "An implementation of  a local feature matching algorithm, which attempts to match multiple views of real-world scene. It uses Harris Corner Detector to get interesting points, SIFT to form local descriptors and ratio or nearest neighbor test for matching";
+export const featureDesc: string = "An implementation of a local feature matching algorithm, which attempts to match multiple views of real-world scene. It uses Harris Corner Detector to get interesting points, SIFT to form local descriptors and ratio or nearest neighbor test for matching.";
 export const featureUrl: string = "/images/feature.png";
 
-export const sceneDesc: string = "•	Created an application that recognized the scenery of an image through a bag of words representation model used with the nearest neighbor and SVM classifiers";
+export const sceneDesc: string = "Created an application that recognized the scenery of an image through a bag of words representation model used with the nearest neighbor and SVM classifiers.";
 export const sceneUrl: string = "/images/scene.png";
 
-export const decisionDesc: string = "Machine Learning implementation of ID3 Algorithm with > 95% accuracy on test data";
+export const decisionDesc: string = "Machine Learning implementation of ID3 Algorithm with > 95% accuracy on test data.";
 export const decisionUrl: string = "/images/decision.jpg";
 
-export const weenixDesc: string = "A Linux-like OS from scratch in C. Used S5FS as File System, which is backed by a virtual file system. Implemented multithreading, essential drivers, and virtual memory for the system.";
+export const weenixDesc: string = "A Linux-like OS from scratch in C. Used S5FS as File System, which is backed by an implemented virtual file system. Implemented multithreading, essential drivers, and virtual memory for the system.";
 export const weenixUrl: string = "/images/weenix.png";
 
 export const inTurnDesc: string = "A Web Application that gives users tools to track their job application status in a more automated way. It uses a chrome extension, which requires users to sign up using google account. After the sign-up, if filling an internship application that uses greenhouse.io, the extension will detect the application. The user information will be automatically filled through the database. After application is submitted, the application information like job title, company, date applied, and status of your application automatically shows up in a list item in the home page of the web application. The extension captures this application automatically and puts it into database for the current user as well. You can filter through the applications dynamically using the search bar, on the home view. Inside the home view, you can manually add new job and it'll be added to database and the list. You can also update the status of added job.";
 export const inTurnUrl: string = "/images/inTurn.png";
 
-export const camDesc: string = "This app will grant a quick way to analyze images, instead of going through exhaustive Google searches to get description of objects present. Fast RCNN is used to segment images and create bounding boxes. On click to bounding boxes the information found on the objects are displayed. Users will be able to query their image using the web application for the project.";
+export const camDesc: string = "This app will grant a quick way to analyze images, instead of going through exhaustive Google searches to get description of objects present. Fast RCNN is implemented to segment images and create bounding boxes. On click to bounding boxes the information found on the objects are displayed. Users will be able to query their image using the web application for the project.";
 export const camUrl: string = "/images/cam.png";
